59e60472d0faf45378e6165fdd63400d64e5b3dc
knakatasf <knakata@dons.usfca.edu>
2025-04-17 21:39:31 -0700

    feature(DeficitRoundRobin): Partially implemented the logic of DeficitRoundRobin

--- model/TrafficClass.h
+++ model/TrafficClass.h
@@ -15,9 +15,10 @@ private:
     uint32_t packets;
     uint32_t maxPackets;
     double weight;
+    uint32_t quantum;
     uint32_t priority_level;
     bool isDefault;
-    std::queue<ns3::Ptr<ns3::Packet> > m_queue;
+    std::queue<ns3::Ptr<ns3::Packet>> m_queue;
     std::vector<Filter> filters;
 
 public:
@@ -29,9 +30,17 @@ public:
 
     bool match(ns3::Ptr<ns3::Packet> p);
 
+    ns3::Ptr<const ns3::Packet> peek() const {
+        if (m_queue.empty()) {
+            return nullptr;
+        }
+        return m_queue.front();
+    }
+
     uint32_t GetPriority() const { return priority_level; }
 
-    double GetQuantum() const { return weight; }
+    uint32_t GetQuantum() const { return quantum; }
+    void AddQuantum(uint32_t quantum) { quantum += quantum; }
 
     void AddFilter(const Filter &f);
 
